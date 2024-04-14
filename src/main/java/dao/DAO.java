6cb96cabe8fd50950557682b2f8f0552f1fac32c
nailtonvital <nailtonvital35@gmail.com>
2024-04-14 16:56:37 -0300

    update

--- src/main/java/dao/DAO.java
+++
@@ -1,8 +0,0 @@
-package dao;
-
-public interface DAO <T extends Object>{
-    public void create(T object);
-    public T read(T object);
-    public void update(T object);
-    public void delete(T object);
-}
